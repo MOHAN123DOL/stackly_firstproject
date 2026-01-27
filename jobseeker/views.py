59307e5907b449806c0432b8c9df05444fb982d1
Mohan V <mohanvenkateshkumarl@gmail.com>
2026-01-27 17:19:41 +0530

    for login

--- jobseeker/views.py
+++ jobseeker/views.py
@@ -266,3 +266,10 @@ class LogoutAPI(APIView):
         )
 
 
+# views.py
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAdminUser
+from .models import UserAppliedJob, UserSavedJob
+
